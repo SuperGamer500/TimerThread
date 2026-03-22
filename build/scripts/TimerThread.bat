d567d7b0340354ae021e52b6dd2b86bf7cecfd74
SuperGamer500 <SuperGamer300.1957@gmail.com>
2026-03-22 18:03:35 -0400

    Version Change

--- build/scripts/TimerThread.bat
+++ build/scripts/TimerThread.bat
@@ -19,7 +19,7 @@
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
-@rem  untitled startup script for Windows
+@rem  TimerThread startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -35,7 +35,7 @@ set APP_HOME=%DIRNAME%..
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and UNTITLED_OPTS to pass JVM options to this script.
+@rem Add default JVM options here. You can also use JAVA_OPTS and TIMER_THREAD_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
@@ -70,22 +70,22 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\untitled-1.0-SNAPSHOT.jar
+set CLASSPATH=%APP_HOME%\lib\TimerThread-1.0-SNAPSHOT.jar
 
 
-@rem Execute untitled
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %UNTITLED_OPTS%  -classpath "%CLASSPATH%"  %*
+@rem Execute TimerThread
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %TIMER_THREAD_OPTS%  -classpath "%CLASSPATH%" com.supergamer.Main %*
 
 :end
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
-rem Set variable UNTITLED_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable TIMER_THREAD_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
-if not ""=="%UNTITLED_EXIT_CONSOLE%" exit %EXIT_CODE%
+if not ""=="%TIMER_THREAD_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
 
 :mainEnd
